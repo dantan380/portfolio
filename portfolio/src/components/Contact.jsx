dc903d7bc3fdad5f185658940dc2af5f56a31abd
Daniel Tan <danieltan380@gmail.com>
2024-07-10 16:55:56 -0700

    Fill out and fully style contact component.

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -1,7 +1,14 @@
+import { CONTACT } from "../constants";
+
 const Contact = () => {
   return (
     <div className="pb-4">
-      Contact
+      <h2 className="my-10 text-center text-4xl">Get in Touch</h2>
+      <div className="text-center tracking-tighter">
+        <p className="my-4">{CONTACT.address}</p>
+        <p className="my-4">{CONTACT.phoneNo}</p>
+        <a href="#" className="border-b">{CONTACT.email}</a>
+      </div>
     </div>
   )
 }
