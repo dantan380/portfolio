17ddb33181d89b10b8dc26bdbc64162144b56410
Daniel Tan <danieltan380@gmail.com>
2024-07-10 15:27:21 -0700

    Add technolgies component to App.

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import Technologies from "./components/Technologies";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Navbar />
         <HeroSection />
         <About />
+        <Technologies />
       </div>
     </div>
   )
