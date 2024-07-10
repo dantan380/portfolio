bd2cd7c16c463e2847a2514b904d843368995506
Daniel Tan <danieltan380@gmail.com>
2024-07-10 16:46:03 -0700

    Add styling changes to adjust for large screens.

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 item-center">
-        <img src={logo} alt="logo" height="35" width="35"></img>
+        <img src={logo} alt="logo" height="35" width="35" className="m-8"></img>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
