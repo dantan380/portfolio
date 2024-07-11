fcbc74a14c6d23cb1fa32911efacf1ad9e92b496
Daniel Tan <danieltan380@gmail.com>
2024-07-10 19:40:44 -0700

    Add animations to contact component.

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -1,15 +1,20 @@
 import { CONTACT } from "../constants";
+import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
-    <div className="pb-4">
+    <motion.div 
+    initial={{ opacity: 0 }}
+    whileInView={{ opacity: 1 }}
+    transition={{ duration: 0.7, delay: 0.5 }}
+    className="pb-4">
       <h2 className="my-10 text-center text-4xl">Get in Touch</h2>
       <div className="text-center tracking-tighter">
         <p className="my-4">{CONTACT.address}</p>
         <p className="my-4">{CONTACT.phoneNo}</p>
         <a href="#" className="border-b">{CONTACT.email}</a>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
