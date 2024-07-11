0a5be1be4b265238409dd78d094895ec7accc829
Daniel Tan <danieltan380@gmail.com>
2024-07-10 19:33:38 -0700

    Add animations to Projects component.

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,21 +1,36 @@
 import { PROJECTS } from "../constants";
+import { motion } from "framer-motion";
 
 const Projects = () => {
   return (
     <div className="pb-4">
-     <h2 className="my-20 text-center text-4xl">Projects</h2>
+      <motion.h2 
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 0.5, delay: 0.2 }}
+     className="my-20 text-center text-4xl">
+      Projects
+      </motion.h2>
      <div>
       {PROJECTS.map((project, index) => (
         <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-          <div className="w-full lg:w-1/4">
+          <motion.div 
+          initial={{ x: -100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+          className="w-full lg:w-1/4">
             <img 
             src={project.image} 
             alt={project.title} 
             width={150} 
             height={150} 
             className="mb-6 rounded"></img>
-          </div>
-          <div className="w-full max-w-xl lg:w-3/4">
+          </motion.div>
+          <motion.div 
+          initial={{ x: 100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+          className="w-full max-w-xl lg:w-3/4">
             <h6 className="mb-2 font-semibold">{project.title}</h6>
             <p className="mb-4">{project.description}</p>
             {project.technologies.map((tech, index) => (
@@ -24,7 +39,7 @@ const Projects = () => {
                 {tech}
               </span>
             ))}
-          </div>
+          </motion.div>
         </div>
       ))}
      </div>
