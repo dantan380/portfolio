1c3c43c82d834d68890b5b82d93e9eb1627de1a1
Daniel Tan <danieltan380@gmail.com>
2024-07-10 23:40:44 -0700

    Change background color.

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -9,7 +9,7 @@ import WorkExperience from "./components/WorkExperience";
 const App = () => {
   return (
     <div className="overflow-x-hidden antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="fixed inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="fixed bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#C5DDFF,transparent)]"></div></div>
+      <div className="fixed inset-0 -z-10 h-full w-full bg-neutral-100 bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="fixed bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#C5DDFF,transparent)]"></div></div>
       <div className="container mx-auto px-8">
         <Navbar />
         <HeroSection />
