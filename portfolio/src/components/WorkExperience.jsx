a777976e014cb125fa742730e08ae4ac8d95c0f8
Daniel Tan <danieltan380@gmail.com>
2024-07-10 19:39:05 -0700

    Add animations to work experience component.

--- portfolio/src/components/WorkExperience.jsx
+++ portfolio/src/components/WorkExperience.jsx
@@ -1,4 +1,5 @@
 import { EXPERIENCES } from "../constants";
+import { motion } from "framer-motion";
 
 const WorkExperience = () => {
   return (
@@ -6,14 +7,22 @@ const WorkExperience = () => {
       <h2 className="my-20 text-center text-4xl">Work Experience</h2>
       <div>
         {EXPERIENCES.map((experience, index) => (
-          <div
+          <motion.div
             key={index}
             className="mb-8 flex flex-wrap lg:justify-center"
             >
-            <div className="w-full lg:w-1/4">
+            <motion.div 
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 0.3 }}
+            className="w-full lg:w-1/4">
               <p className="mb-2 text-sm">{experience.year}</p>
-            </div>
-            <div className="w-full max-w-xl lg:w-3/4">
+            </motion.div>
+            <motion.div 
+            initial={{ opacity: 0, x: 100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 0.3 }}
+            className="w-full max-w-xl lg:w-3/4">
               <h6 className="mb-2 font-semibold">
                 {experience.role} -{" "} <span className="text-sm text-blue-400">
                   {experience.company}
@@ -27,8 +36,8 @@ const WorkExperience = () => {
                   {tech}
                 </span>
               ))}
-            </div>
-          </div>
+            </motion.div>
+          </motion.div>
         ))}
       </div>
     </div>
