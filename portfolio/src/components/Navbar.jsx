208f9a9994ebc06582a273de1018ca84325b2374
Daniel Tan <danieltan380@gmail.com>
2024-07-10 20:36:08 -0700

    Add links to github and linkedin for logos.

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import logo from "../assets/dt-high-resolution-logo-black-transparent.png"
 import { FaLinkedin } from "react-icons/fa"
 import { FaGithub } from "react-icons/fa"
+import { LOGO } from "../constants"
 
 const Navbar = () => {
   return (
@@ -9,8 +10,12 @@ const Navbar = () => {
         <img src={logo} alt="logo" height="35" width="35" className="m-8"></img>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
+      <a href={LOGO.github.link}>
         <FaGithub />
+      </a>
+      <a href={LOGO.linkedin.link}>
+      <FaLinkedin />
+      </a>
       </div>
     </nav>
   )
